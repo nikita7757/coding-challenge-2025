54d1c6a0670196a171eb47a923031a601bc7d158
Nikita Shedage <132663150+nikita7757@users.noreply.github.com>
2025-08-17 23:08:15 +0530

    Create FindDuplicate.java

---
+++ FindDuplicate.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class FindDuplicate {
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int duplicate = findDuplicate(arr);
+        System.out.println("Duplicate number: " + duplicate);
+    }
+}
